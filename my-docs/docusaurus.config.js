d611ac1c4156085a593a62b1013d20105ebc8dc2
Wez Lemington <wezreal@outlook.com>
2026-04-11 12:11:29 +0100

    Update docusaurus.config.js

--- my-docs/docusaurus.config.js
+++ my-docs/docusaurus.config.js
@@ -99,26 +99,14 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
+                href: 'https://discordapp.com/invite/deltacommunity',
               },
             ],
           },
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/wezlemin/delta-docs',
