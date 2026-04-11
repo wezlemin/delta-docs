678bce8d6a5f149357b10dde16884114c90502f1
Wez Lemington <wezreal@outlook.com>
2026-04-11 12:20:44 +0100

    Update index.js

--- my-docs/src/pages/index.js
+++ my-docs/src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import { useDocusaurusContext } from '@docusaurus/useDocusaurusContext';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
@@ -10,7 +10,6 @@ export default function Home() {
       <main style={{ textAlign: 'center', padding: '100px 20px' }}>
         <h1>Welcome to {siteConfig.title}!</h1>
 
-        {/* 👇 THIS uses your tagline from config */}
         <p>{siteConfig.tagline}</p>
 
         <a
