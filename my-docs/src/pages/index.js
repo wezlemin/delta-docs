787b6fea5e4a95fadcf9e42cb770bf4387074494
Wez Lemington <wezreal@outlook.com>
2026-04-11 01:48:22 +0100

    Update index.js

--- my-docs/src/pages/index.js
+++ my-docs/src/pages/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import { Redirect } from '@docusaurus/router';
+import React, { useEffect } from 'react';
+import { useHistory } from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/docs/intro" />;
+  const history = useHistory();
+
+  useEffect(() => {
+    history.replace('/docs/intro');
+  }, [history]);
+
+  return null;
 }
