a67d279eda4aa263751d8783598b2c926a8489a7
Wez Lemington <wezreal@outlook.com>
2026-04-11 12:18:33 +0100

    Update docusaurus.config.js

--- my-docs/docusaurus.config.js
+++ my-docs/docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Delta Docs',
-  tagline: 'Unofficial',
+  tagline: 'Welcome to this unofficial documentation!',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
